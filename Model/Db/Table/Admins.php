444a96e0b4880eebdc0a7b70f8e87d66f2509757
Appteam <info@appteam.it>
2024-06-01 19:49:04 +0200

    Update

--- Model/Db/Table/Admins.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Xdelivery_Model_Db_Table_Admins extends Core_Model_Db_Table {
-    protected $_name                    = "xdelivery_admins";
-    protected $_primary                 = "id";
- 
-}
