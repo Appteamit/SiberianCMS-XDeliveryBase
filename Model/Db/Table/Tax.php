9fca01d112dfbd606511080c95dd571acb6c83d1
Appteam.it <info@spotcloud.it>
2024-06-01 19:29:00 +0200

    Add files via upload

---
+++ Model/Db/Table/Tax.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Class Xdelivery_Model_Db_Table_Tax
+ * @package Xdelivery\Model\Db\Table
+ */
+class Xdelivery_Model_Db_Table_Tax extends Core_Model_Db_Table
+{
+
+    protected $_name                    = "xdelivery_taxes";
+    protected $_primary                 = "id";
+
+	
+}
---
+++ Model/Store.php
@@ -0,0 +1,36 @@
+<?php
+
+class Xdelivery_Model_Store extends Core_Model_Default {
+
+    /**
+     * @var null
+     */
+    public static $acl = null;
+
+    /**
+     * @var string
+     */
+    protected $_db_table = Xdelivery_Model_Db_Table_Store::class;
+
+
+    /**
+     * @param $valuesId
+     * @param array $params
+     * @return Xdelivery_Model_Store[]
+     */
+    public function findByValueId($valuesId, $params = [])
+    {
+        return $this->getTable()->findByValueId($valuesId, $params);
+    }
+
+    /**
+     * @param $valuesId
+     * @param array $params
+     * @return Xdelivery_Model_Store[]
+     */
+    public function countAllForApp($valuesId, $params = [])
+    {
+        return $this->getTable()->countAllForApp($valuesId, $params);
+    }
+   
+}
